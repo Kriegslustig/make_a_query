92aa32b34d43f54b204056774f7b2e4ea05bc85a
Kriegslustig <github@ls7.ch>
2014-12-03 16:23:24 +0100

    Adding some comments and nicening up some stuff

--- make_a_query.php
+++ make_a_query.php
@@ -18,12 +18,29 @@ require 'maq_settings.php';
 // [make_a_query type="post" cat="category_slug" limit=5]
 function add_sc_make_a_query ($attributes) {
 
-  // this array maps attributes to their function
+  // Defining shortcode defaults
+  $attributes = shortcode_atts([
+    'type' => 'post',
+    'cat' => false,
+    'limit' => false,
+    'template' => false
+  ], $attributes, 'make_a_query');
+
+  /*
+   * this array maps shortcode attributes to their processors
+   * The processor the handles the attribute
+   * Most attributes are integrated into the $wp-query
+   */
   $make_a_query_func_arr = [];
 
-  // The users teplate should be in this dir
 
+  /*
+   * Searches a Template File
+   * Fallback: default-post-template.php
+   */
   function maq_eval_template_path($attributes) {
+
+    // The users teplate should be in this dir
     $user_dir = get_option('make_a_query_templates_directory');
     $user_dir = $user_dir ?
       $user_dir :
@@ -38,6 +55,7 @@ function add_sc_make_a_query ($attributes) {
     return plugin_dir_path(__FILE__) . '/default-post-template.php';
   }
 
+  // Process the template attribute
   $make_a_query_func_arr['template'] = 'maq_att_template';
   function maq_att_template ($wp_query_arr, $val) {
     if($val) {
@@ -46,12 +64,14 @@ function add_sc_make_a_query ($attributes) {
     return $wp_query_arr;
   }
 
+  // Process the type attribute
   $make_a_query_func_arr['type'] = 'maq_att_type';
   function maq_att_type ($wp_query_arr, $val) {
     $wp_query_arr['post_type'] = $val;
     return $wp_query_arr;
   }
 
+  // Process the cat attribute
   $make_a_query_func_arr['cat'] = 'maq_att_cat';
   function maq_att_cat ($wp_query_arr, $val) {
     if($val) {
@@ -60,6 +80,7 @@ function add_sc_make_a_query ($attributes) {
     return $wp_query_arr;
   }
 
+  // Process the limit attribute
   $make_a_query_func_arr['limit'] = 'maq_att_limit';
   function maq_att_limit ($wp_query_arr, $val) {
     if($val) {
@@ -67,17 +88,13 @@ function add_sc_make_a_query ($attributes) {
     }
     return $wp_query_arr;
   }
-  $attributes = shortcode_atts([
-    'type' => 'post',
-    'cat' => false,
-    'limit' => false,
-    'template' => false
-  ], $attributes, 'make_a_query');
+
   $wp_query_arr = [];
   foreach ($attributes as $att => $val) {
     $wp_query_arr = call_user_func($make_a_query_func_arr[$att], $wp_query_arr, $val);
   }
   $wp_query = new WP_Query($wp_query_arr);
+
   // Look for a valid template_file
   $template_file = maq_eval_template_path($attributes);
   ob_start();
