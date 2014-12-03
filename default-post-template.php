5f5a4697776ad1f0d2499aafaff80924fc6296f9
Kriegslustig <github@ls7.ch>
2014-12-03 16:24:18 +0100

    Adding comments to the default template

--- default-post-template.php
+++ default-post-template.php
@@ -1,6 +1,9 @@
 <?php
+  // Check if '$wp_query' is defined and if there are any posts returned 
   if($wp_query && $wp_query->have_posts()):
+    // Initiate the_loop
     while($wp_query->have_posts()):
+      // Call 'the_post()' to set the environment for 
       $wp_query->the_post();
 ?>
       <article>
